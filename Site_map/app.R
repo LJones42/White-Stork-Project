f02500f138ff25fbfe01b8b0b9f6f3984d6482e8
Lizzie Jones <lizziejones@Lizzies-MBP.lan>
2021-06-09 15:08:41 +0100

    Final commit of week 6 of post-doc. Small alterations to ensure Markdowns can be run and uploaded to OneDrive

--- Site_map/app.R
+++ Site_map/app.R
@@ -33,10 +33,6 @@ ui <- fluidPage(
 # Create map as server
 server <- function(input, output, session) {
     
-    points <- eventReactive(input$recalc, {
-        cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-    }, ignoreNULL = FALSE)
-    
     output$mymap <- renderLeaflet({
         leaflet(marker_df) %>% 
             addTiles() %>%
