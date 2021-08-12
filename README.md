3acb183f7037d2589d78b068540efa72a97a9878
Lizzie Jones <lizziejones@Lizzies-MBP.lan>
2021-08-12 16:24:41 +0100

    refactored project structure

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Thoughts-on-Storks Project Code
+# White-Stork-Project Code
 
 This is the readme.md file for the White Stork Project code (written by Lizzie Jones). Code is stored in privately in github and pushed daily, then reviewed before merging with the master. rMarkdown files are created periodically as word doucments and added to the main 'results' GoogleDoc.
---
+++ Shiny_app_map/app.R
@@ -0,0 +1,53 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+### Creating a OpenStreetMap of Knepp estate
+
+# Call relevant packages (assuming installed)
+library(shiny)
+library(leaflet)
+library(htmltools)
+library(shinydashboard)
+library(shiny)
+
+# Define data frame of site names and coordinates
+marker_df <- read.csv(textConnection(
+    "Name,Lat,Long
+Knepp,50.98341,-0.35485
+Wadhurst,51.03579,0.32769
+Wintershall,51.16605,-0.55289"))
+
+# Define the UI for the Shiny app
+ui <- fillPage(
+    leafletOutput("mymap", height = "100%"),
+    p(),
+    actionButton("recalc", "New points")
+)
+
+# Create map as server
+server <- function(input, output, session) {
+    
+    output$mymap <- renderLeaflet({
+        leaflet(marker_df) %>% 
+            
+            addTiles() %>%
+            setView(lng=-0.35485, lat=50.98341, zoom = 10) %>% 
+            addMarkers(lng=~Long, lat=~Lat, popup = ~htmltools::htmlEscape(Name)) %>%
+            addCircles(lng=-0.35485, lat=50.98341, color = "red", radius = 15000) %>% 
+            addCircles(lng=0.32769, lat=51.03579, color = "blue", radius = 15000) %>% 
+            addCircles(lng=-0.55289, lat=51.16605, color = "green", radius = 15000) %>% 
+            addMeasure() %>%
+            addMiniMap()
+    })
+}
+
+# Run the Shiny app map
+shinyApp(ui, server)
+
